c35df135bd15f3e96396774ef2bb948b8e169461
ruucm <ruucm.a@gmail.com>
2019-12-10 01:26:45 +0900

    add ReduxPage

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import './app.scss'
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./app.scss";
 
-import App from './App'
-import ReactBasics from './ReactBasics'
-import Framer from './Framer'
-import Flex from './Flex'
+import App from "./App";
+import ReactBasics from "./ReactBasics";
+import Framer from "./Framer";
+import Flex from "./Flex";
+import Redux from "./Redux";
 
 function AppRouter() {
   return (
@@ -26,15 +27,19 @@ function AppRouter() {
           <li>
             <Link to="/flex/">Flex</Link>
           </li>
+          <li>
+            <Link to="/Redux/">ReduxPage</Link>
+          </li>
         </ul>
 
         <Route path="/" exact component={App} />
         <Route path="/react-basics/" component={ReactBasics} />
         <Route path="/framer/" component={Framer} />
         <Route path="/flex/" component={Flex} />
+        <Route path="/Redux/" component={Redux} />
       </div>
     </Router>
-  )
+  );
 }
 
-export default AppRouter
+export default AppRouter;
---
+++ src/Redux/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+function ReduxPage() {
+  return <div>hey</div>;
+}
+
+export default ReduxPage;
