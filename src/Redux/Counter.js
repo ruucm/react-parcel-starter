8794a851346cb1afb99cfa14962788bbff72d2f0
ruucm <ruucm.a@gmail.com>
2019-12-10 01:37:29 +0900

    add Counter Comp

--- src/Redux/Counter.js
+++ src/Redux/Counter.js
@@ -1,26 +1,52 @@
 import React from "react";
+import { connect } from "react-redux";
 
-export function Counter() {
-  return (
-    <div>
-      <h1>react-reducer counter</h1>
+export const Counter = connect(state => ({
+  count: state.counterReducer.count
+}))(({ count, dispatch, ...props }) => (
+  <div>
+    <h1>react-reducer counter</h1>
 
-      <div>count : {this.props.count}</div>
-      <button
-        onClick={() => {
-          this.props.dispatch({ type: "INCREMENT" });
-        }}
-      >
-        INCREMENT
-      </button>
+    <div>count : {count}</div>
+    <button
+      onClick={() => {
+        dispatch({ type: "INCREMENT" });
+      }}
+    >
+      INCREMENT
+    </button>
 
-      <button
-        onClick={() => {
-          this.props.dispatch({ type: "DECREMENT" });
-        }}
-      >
-        DECREMENT
-      </button>
-    </div>
-  );
-}
+    <button
+      onClick={() => {
+        dispatch({ type: "DECREMENT" });
+      }}
+    >
+      DECREMENT
+    </button>
+  </div>
+));
+
+// export function Counter() {
+//   return (
+//     <div>
+//       <h1>react-reducer counter</h1>
+
+//       <div>count : {count}</div>
+//       <button
+//         onClick={() => {
+//           dispatch({ type: "INCREMENT" });
+//         }}
+//       >
+//         INCREMENT
+//       </button>
+
+//       <button
+//         onClick={() => {
+//           dispatch({ type: "DECREMENT" });
+//         }}
+//       >
+//         DECREMENT
+//       </button>
+//     </div>
+//   );
+// }
--- src/Redux/index.js
+++ src/Redux/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { Counter } from "./Counter";
 
 function ReduxPage() {
   return (
     <Provider store={store}>
-      <div>hey</div>
+      <Counter />
     </Provider>
   );
 }
