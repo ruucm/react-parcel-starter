14bd2ae7d3add312964e369536b0bf6c104571af
ruucm <ruucm.a@gmail.com>
2020-02-18 14:32:30 +0900

    add basic react context things

--- src/Context/index.js
+++ src/Context/index.js
@@ -1,29 +1,96 @@
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import Notification from "./Notification";
+import Header from "./Header";
+import { Store } from "./store";
 
 function Comp() {
+  const [state, setState] = useState({
+    notifications: {
+      "1": {
+        id: 1,
+        text: "Sup!",
+        seen: false
+      },
+      "2": {
+        id: 2,
+        text: "Wasup!",
+        seen: false
+      },
+      "3": {
+        id: 3,
+        text: "Waza!",
+        seen: false
+      }
+    }
+  });
   return (
     <React.Fragment>
-      Context Eu nisi cupidatat labore cupidatat tempor reprehenderit
-      reprehenderit labore nisi duis. Aliquip exercitation sint aliquip
-      reprehenderit aliqua officia. Nostrud voluptate esse Lorem aliquip
-      pariatur cillum amet labore irure voluptate aliqua tempor ut. Adipisicing
-      officia enim consequat pariatur cupidatat enim in incididunt pariatur
-      consequat tempor reprehenderit qui deserunt. Duis esse esse velit proident
-      est minim officia. Consectetur eiusmod mollit magna magna est. Laboris
-      aute voluptate dolore deserunt ad. Velit magna est Lorem occaecat amet
-      ullamco adipisicing. Dolore ullamco et ut occaecat reprehenderit ipsum sit
-      minim officia eiusmod id. Qui est esse officia anim eiusmod et et laboris
-      mollit. Velit esse exercitation qui aliqua eiusmod ipsum mollit nulla. Do
-      sunt culpa aliquip culpa incididunt. Ullamco excepteur ut reprehenderit ad
-      occaecat eiusmod culpa ullamco Lorem exercitation ex aliqua veniam mollit.
-      Commodo dolor elit esse ex. Qui esse id et ad veniam tempor cupidatat
-      pariatur cupidatat. Duis aliquip enim commodo elit dolor tempor do
-      deserunt occaecat duis dolor occaecat. Sit laboris eiusmod anim laboris
-      consectetur enim in ex amet Lorem cupidatat consectetur sit. Commodo
-      excepteur cillum veniam enim sint labore nulla nostrud occaecat esse.
-      Occaecat cupidatat nostrud dolore elit proident tempor mollit consectetur
-      eu.
+      {console.log("state!!!", state)}
+      <Store.Provider
+        value={{
+          ...state,
+          deleteNotification: id => {
+            console.log("deleteNotification");
+            console.log("id", id);
+
+            setState({
+              notifications: {
+                "1": {
+                  id: 1,
+                  text: "Sup!",
+                  seen: false
+                },
+
+                "3": {
+                  id: 3,
+                  text: "Waza!",
+                  seen: false
+                }
+              }
+            });
+            // Not work below thing cause it is immutable
+            // setState(prevState => {
+            //   const newState = delete prevState.notifications[id];
+            //   return delete prevState.notifications[id];
+            // });
+          },
+          seeNotification: id => {
+            console.log("seeNotification");
+            console.log("id", id);
+            setState(prevState => {
+              return {
+                ...prevState,
+                notifications: {
+                  ...prevState.notifications,
+                  [id]: {
+                    ...prevState.notifications[id],
+                    seen: true
+                  }
+                }
+              };
+            });
+          }
+        }}
+      >
+        <Header />
+        <Store.Consumer>
+          {store => {
+            console.log("store!!", store);
+            return Object.keys(store.notifications).map(key => {
+              return (
+                <Notification
+                  key={store.notifications[key].id}
+                  id={store.notifications[key].id}
+                  text={store.notifications[key].text}
+                  seen={store.notifications[key].seen}
+                />
+              );
+            });
+          }}
+        </Store.Consumer>
+      </Store.Provider>
     </React.Fragment>
   );
 }
